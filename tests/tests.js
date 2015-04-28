f6124a9ca1d9ed424bb873b1ec9174f50917960d
Daniel Hinds-Bond <dbond@leapfrogonline.com>
2015-04-28 09:23:05 -0500

    write a test that tests something useful

--- tests/tests.js
+++ tests/tests.js
@@ -1,13 +1,26 @@
 var assert = require('chai').assert;
 var server = require('../server.js');
+var http = require('http');
 
 describe('Server Tests', function(){
   before(function(done){
     done();
   });
 
-  it('should run a test', function(){
-    assert.equal(1, 1, 'one equals one');
+  it('return a status of 200', function(done){
+    http.request({
+      'host': '127.0.0.1',
+      'path': '/',
+      'port': '8080'
+    }, function(response){
+      assert(response.statusCode, 200);
+      done();
+    })
+    .on('error', function(error){
+      console.log('error', error);
+      done();
+    })
+    .end();
   });
 
   after(function(done){
